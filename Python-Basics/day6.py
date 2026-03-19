722352c37708d579028d09a4bc4e0dc9f89fb489
Archi Lodha <archilodha1@gmail.com>
2026-03-19 14:47:42 +0530

    Add functions for addition and greeting

---
+++ Python-Basics/day6.py
@@ -0,0 +1,13 @@
+# Day 6 - Functions
+
+def add(a, b):
+    return a + b
+
+result = add(5, 3)
+print("Sum:", result)
+
+# Another function
+def greet(name):
+    print("Hello", name)
+
+greet("Archi")

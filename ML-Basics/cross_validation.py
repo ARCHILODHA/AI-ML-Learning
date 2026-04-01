deb15e5c7c5c2be9465613e5313237f6204c1c50
Archi Lodha <archilodha1@gmail.com>
2026-04-01 06:47:08 +0530

    Add cross-validation implementation using LogisticRegression

---
+++ ML-Basics/cross_validation.py
@@ -0,0 +1,7 @@
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+
+model = LogisticRegression()
+scores = cross_val_score(model, X, y, cv=5)
+
+print(scores.mean())

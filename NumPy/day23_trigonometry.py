773cd60011097a00b7bf00cde4675c30e7648cf8
Archi Lodha <archilodha1@gmail.com>
2026-07-10 08:33:53 +0530

    Add trigonometric functions for given angles

---
+++ NumPy/day23_trigonometry.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+angles = np.array([0, 30, 45, 60, 90])
+
+radians = np.radians(angles)
+
+print("Sin:")
+print(np.sin(radians))
+
+print("\nCos:")
+print(np.cos(radians))
+
+print("\nTan:")
+print(np.tan(radians))

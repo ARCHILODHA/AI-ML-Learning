a915855a9f29d7be618ef2ad01756b3e1de10ce0
Archi Lodha <archilodha1@gmail.com>
2026-08-10 11:38:57 +0530

    Add health check endpoint to FastAPI application

---
+++ Model-Deployment/health_check.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/health")
+def health():
+    return {
+        "status": "Healthy",
+        "message": "API is running successfully."
+    }

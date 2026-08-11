4d1184c580c2db1cf97ed9cd1368c586c39b6a8d
Archi Lodha <archilodha1@gmail.com>
2026-08-11 11:53:20 +0530

    Add arange and linspace examples in new script

---
+++ NumPy/day27_arange_linspace.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr_range = np.arange(0, 20, 2)
+
+arr_linear = np.linspace(0, 1, 6)
+
+print("Array using arange:")
+print(arr_range)
+
+print("\nArray using linspace:")
+print(arr_linear)
